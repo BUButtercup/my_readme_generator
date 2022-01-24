2a549cf2b5913979761546a9963c668274ff35c5
Buttercup <andyandnile@gmail.com>
2022-01-24 00:05:49 -0800

    1. added when functions to applicable prompts, 2. wrote list-generating functions in fun-ctions.js, 3. used functions to process input to install and use instructions into list format. Now testing file generation

--- fun-ctions.js
+++ fun-ctions.js
@@ -1,3 +1,26 @@
-// module.exports {
+function generateUList(ans, arr){
+    for(let i=0; i<ans.length; i++){
+        let listItem = ans[i].trim();
+        const newLI = `- ${listItem}\n`;
+        arr.push(newLI);
+    }
+    arr = arr.join('');
+    console.log(arr);
+}
+function generateOList(ans, arr){
+    for(let i=0; i<ans.length; i++){
+        let listItem = ans[i].trim();
+        let num = i+1
+        const newLI = `${num}. ${listItem}\n`;
+        arr.push(newLI);
+    }
+    arr = arr.join('');
+    console.log(arr);
+}
 
-// };
+
+
+module.exports = {
+    generateUList,
+    generateOList,
+};
